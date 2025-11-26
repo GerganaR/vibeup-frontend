1dded48c9dd34f5d295a4e3192abbad797c7fea6
Gergana Roshleva <gergana.roshleva@gmail.com>
2025-11-26 22:02:25 +0200

    Configures Tailwind CSS with Material Tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,182 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Sofia Sans", "sans-serif"],
+      condensed: ["Sofia Sans Condensed", "sans-serif"],
+    },
+    colors: {
+      gray: {
+        300: "#D0D5DD",
+        500: "#667085",
+      },
+      // Neutral Palette
+      neutral: {
+        black: "#000000",
+        white: "#FEFFFF",
+        grey25: "#F9FAFA",
+        grey50: "#F1F2F2",
+        grey100: "#E4E6E6",
+        grey200: "#CBCCCC",
+        grey300: "#B5B6B6",
+        grey400: "#989999",
+        grey500: "#878686",
+        grey600: "#666565",
+        grey700: "#4D4C4C",
+        grey800: "#333333",
+        grey900: "#1A1919",
+      },
+      // Blue Palette
+      blue: {
+        50: "#F2FAFF",
+        100: "#E6F6FF",
+        200: "#D1EEFF",
+        300: "#ACD0E6",
+        400: "#5CA1CC",
+        500: "#2D80B3",
+        600: "#1A5980",
+        700: "#043959",
+        800: "#022133",
+        900: "#ACD0E5",
+      },
+      // Turquoise Palette
+      turquoise: {
+        50: "#F7FBFC",
+        100: "#EDF6FA",
+        200: "#DAEBF2",
+        300: "#ADCCD9",
+        400: "#86AEBF",
+        500: "#6392A6",
+        600: "#3D7E99",
+        700: "#206481",
+        800: "#003F59",
+      },
+      // Red Palette (Error)
+      red: {
+        50: "#FFFAFA",
+        100: "#FEE2E2",
+        200: "#FFCCCD",
+        300: "#FFA6A7",
+        400: "#EA7576",
+        500: "#EF4444",
+        600: "#DC2626",
+        700: "#B43328",
+        800: "#80241C",
+      },
+      // Orange Palette (Warning)
+      orange: {
+        50: "#FAF5EE",
+        100: "#FAEDD4",
+        200: "#FFDCA8",
+        300: "#FABF7D",
+        400: "#FBAB63",
+        500: "#FC8921",
+        600: "#D16300",
+        700: "#9E5400",
+        800: "#6E3B00",
+      },
+      // Green Palette (Normal status)
+      green: {
+        50: "#ECFDF3",
+        100: "#D1FADF",
+        200: "#A6F4C5",
+        300: "#6CE9A6",
+        400: "#32D583",
+        500: "#12B76A",
+        600: "#039855",
+        700: "#027A48",
+        800: "#054F31",
+      },
+      purple: {
+        50: "#F8F9FC",
+        100: "#EAECF5",
+        200: "#D5D9EB",
+        300: "#AFB5D9",
+        400: "#717BBC",
+        500: "#4E5BA6",
+        600: "#3E4784",
+        700: "#363F72",
+        800: "#293056",
+      },
+      slate: {
+        50: "#F8FAFC",
+        100: "#F1F5F9",
+        200: "#E2E8F0",
+        300: "#CBD5E1",
+        400: "#94A3B8",
+        500: "#64748B",
+        600: "#475569",
+        700: "#334155",
+        800: "#1E293B",
+        900: "#0F172A",
+        950: "#020617",
+      },
+      yellow: {
+        500: "#EAB308",
+      },
+      amber: {
+        100: "#FEF3C7",
+        500: "#F59E0B",
+        600: "#D97706",
+      },
+      emerald: {
+        100: "#D1FAE5",
+        500: "#10B981",
+        600: "#059669",
+      },
+      teal: {
+        500: "#14B8A6",
+      },
+      primary: {
+        blue500: "#2D80B3",
+      },
+    },
+    extend: {
+      backgroundImage: {
+        // Gradients
+        "red-purple": "linear-gradient(93.96deg, #FFFAFA 0%, #F1F2F2 100%)",
+        "dark-blue-dark-turquoise":
+          "linear-gradient(117.03deg, #043959 0%, #043959 0.01%, #003F59 100%)",
+        "turquoise-turquoise":
+          "linear-gradient(117.03deg, #EDF6FA 0%, #D1FADF 100%)",
+        "light-blue-blue":
+          "linear-gradient(117.03deg, #2D80B3 0%, #2D80B3 0.01%, #5CA1CC 100%)",
+        "main-gradient": "linear-gradient(117deg, #F7FBFC 0%, #F9FAFA 100%)",
+      },
+    },
+    screens: {
+      sm: "320px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "1740px",
+      // => @media (min-width: 1740px) { ... }
+
+      "4xl": "1820px",
+      // => @media (min-width: 1820px) { ... }
+
+      "5xl": "1920px",
+      // => @media (min-width: 1920px) { ... }
+
+      "6xl": "2100px",
+      // => @media (min-width: 2100px) { ... }
+    },
+  },
+
+  plugins: [],
+});
